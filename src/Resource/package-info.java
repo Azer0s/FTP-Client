2488b63921d635e6d49622bc31da22bb6b9c0839
Azer0s <sim17057@spengergasse.at>
2016-06-02 14:21:23 +0200

    Added exe-wrapper configuration

--- src/Resource/package-info.java
+++ src/Resource/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package contains all resources such as FXML files and images.
+ * Package contains all resources such as FXML files, icons and the exe-wrapper configuration.
  */
 package Resource;
